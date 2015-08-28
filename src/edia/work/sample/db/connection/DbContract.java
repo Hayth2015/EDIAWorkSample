122eacfe2b8ad5ffed60c4f8095574d1b86312db
Hayth2015 <haytham.nasri@gmail.com>
2015-08-28 11:11:32 +0200

    Test Database connexion

---
+++ src/edia/work/sample/db/connection/DbContract.java
@@ -0,0 +1,14 @@
+package edia.work.sample.db.connection;
+
+
+public interface DbContract {
+
+	public static final String HOST = "jdbc:postgresql://localhost:5432/";
+
+	public static final String DB_NAME = "EDIA_Work_Sample";
+
+	public static final String USERNAME = "postgres";
+
+	public static final String PASSWORD = "admin";
+
+}
